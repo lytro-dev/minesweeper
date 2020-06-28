3b1af802be6c2c7dc09eeb242aa99473d7cee5f3
lytro-dev <stevebayard226@gmail.com>
2020-06-28 15:04:45 -0400

    initial project files

---
+++ src/components/MineField.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MineField = () => {
+    return(<div>Mine Field goes here</div>)
+}
+
+export default MineField
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import MineField from './MineField'
+
+export {
+    MineField
+}
