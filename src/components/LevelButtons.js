18dcc235cb8f5143e5e681c3483c19837ac276d0
lytro-dev <stevebayard226@gmail.com>
2020-07-09 22:19:42 -0400

    add styling to level buttons

--- src/components/LevelButtons.js
+++ src/components/LevelButtons.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const LevelButtons = () => {
+
+    const handleBeginnerClick = () => {
+
+    }
+
+    const handleIntermediateClick = () => {
+        
+    }
+
+    const handleExpertClick = () => {
+        
+    }
     return(<div className="level-buttons">
-        Level Buttons go here
+        <button className="button-main" onClick={handleBeginnerClick}>Beginner</button>
+        <button className="button-main" onClick={handleIntermediateClick}>Intermediate</button>
+        <button className="button-main" onClick={handleExpertClick}>Expert</button>
     </div>)
 }
 
